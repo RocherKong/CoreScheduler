01b22aab29b899118dc1ca7ab0769310756c728c
Rocher <rocher@126.com>
2018-04-25 14:39:56 +0800

    Add MonthUnit

---
+++ CoreScheduler/Unit/MonthUnit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreScheduler.Unit
+{
+    public class MonthUnit : UnitBase, ITimeRestrictableUnit
+    {
+        public MonthUnit(Schedule schedule, int duration) : base(schedule, duration)
+        {
+            this._schedule.CalculateNextRunTime = x => x.AddMonths(duration);
+        }
+
+        public Schedule GetSchedule => this._schedule;
+    }
+}
